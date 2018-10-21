e078f429816d23e09b0e5e1a81845c1019276f77
Edwin Montoya Munera <emontoya@eafit.edu.co>
2018-10-21 18:25:58 -0500

    mpi python

--- mpi4py/mpi4py-examples/executor.sh
+++ mpi4py/mpi4py-examples/executor.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+if ! [ -x /usr/bin/nproc ]; then
+    echo "nproc is not installed. Please install it."
+    exit 1
+fi
+
+PYTHON_HOME=/opt/anaconda3/bin
+CORES=$(nproc)
 EXAMPLE=09
-CORES=4
-mpiexec -np ${CORES} python ./${EXAMPLE}*.py
+mpiexec -f ../hosts_mpi -np ${CORES} ${PYTHON_HOME}/python ./${EXAMPLE}*.py
